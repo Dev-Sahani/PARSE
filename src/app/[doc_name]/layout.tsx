77d3ea1b8d88ef0b06a8fd9e721d12ae12a01cfc
Dev-Sahani <devdaksh8102003@gmail.com>
2024-09-04 00:12:37 +0530

    upload file completed

--- src/app/[doc_name]/layout.tsx
+++ src/app/[doc_name]/layout.tsx
@@ -2,10 +2,18 @@ import { ReactNode } from "react";
 import Sidebar from "@/components/ui/Sidebar";
 import Navbar from "@/components/ui/Navbar";
 
-export default function ReportLayout({ children }: { children: ReactNode }) {
+export default function ReportLayout({
+  children,
+  params,
+}: {
+  children: ReactNode;
+  params: {
+    filename: string;
+  };
+}) {
   return (
     <div className="flex">
-      <Sidebar />
+      <Sidebar filename={params.filename} />
       <div className="w-full">
         <Navbar />
         <main className="p-6 pt-0">{children}</main>
--- src/app/[doc_name]/overview/page.tsx
+++ src/app/[doc_name]/overview/page.tsx
@@ -1,52 +1,65 @@
+import { getBasicDetails } from "@/app/api/getOverviewData";
 import BasicDetails from "./BasicDetails";
 import OtherImages from "./OtherImages";
 import TextualAnalysis from "./TextualAnalysis";
 
-export default function OverviewPage() {
-  return <div>
-    <div className="my-4">
-      <h3 className="text-xl font-semibold">Basic Details</h3>
-      <BasicDetails data={data}/>
+export default async function OverviewPage({
+  params,
+}: {
+  params: { doc_name: string };
+}) {
+  // console.log(params.doc_name);
+  // if (params.doc_name) {
+  //   const res = await getBasicDetails(params.doc_name);
+  //   console.log(res);
+  // }
+
+  return (
+    <div>
+      <div className="my-4">
+        <h3 className="text-xl font-semibold">Basic Details</h3>
+        <BasicDetails data={data} />
+      </div>
+      <div className="my-8">
+        <h3 className="text-xl font-semibold">Other Images</h3>
+        <OtherImages data={data} />
+      </div>
+      <div className="my-4">
+        <h3 className="text-xl font-semibold">Textual Analysis</h3>
+        <TextualAnalysis data={data} />
+      </div>
     </div>
-    <div className="my-8">
-      <h3 className="text-xl font-semibold">Other Images</h3>
-      <OtherImages data={data}/>
-    </div>
-    <div className="my-4">
-      <h3 className="text-xl font-semibold">Textual Analysis</h3>
-      <TextualAnalysis data={data}/>
-    </div>
-  </div>;
+  );
 }
 
 export type Data = {
-    ProductName: string,
-    Category: string,
-    Sub_category: string,
-    Price: number,
-    Rating: number,
-    No_rating: number,
-    Discount: number,
-    M_Spend: number,
-    Supply_Chain_E: number,
-    Sales_y: number,
-    Sales_m: number,
-    Market_T: number,
-    Seasonality_T: number  
-}
+  ProductName: string;
+  Category: string;
+  Sub_category: string;
+  Price: number;
+  Rating: number;
+  No_rating: number;
+  Discount: number;
+  M_Spend: number;
+  Supply_Chain_E: number;
+  Sales_y: number;
+  Sales_m: number;
+  Market_T: number;
+  Seasonality_T: number;
+};
 
 const data = {
-  "ProductName": "CODE Sunscreen Gel Creme",
-  "Category": "Beauty & Health",
-  "Sub_category": "Skincare",
-  "Price": 315,
-  "Rating": 4.6,
-  "No_rating": 5,
-  "Discount": 10,
-  "M_Spend": 2200,
-  "Supply_Chain_E": 78.0,
-  "Sales_y": 10600,
-  "Sales_m": 883,
-  "Market_T": 3.5,
-  "Seasonality_T": 8.0
+  ProductName: "CODE Sunscreen Gel Creme",
+  Category: "Beauty & Health",
+  Sub_category: "Skincare",
+  Price: 315,
+  Rating: 4.6,
+  No_rating: 5,
+  Discount: 10,
+  M_Spend: 2200,
+  Supply_Chain_E: 78.0,
+  Sales_y: 10600,
+  Sales_m: 883,
+  Market_T: 3.5,
+  Seasonality_T: 8.0,
 };
